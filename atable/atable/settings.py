8ecbc93382b19f6497191efa7a694a99b3479c60
tr4ck3ur <tr4ck3ur@style-python.fr>
2015-05-10 02:50:29 +0200

    Fix static file link

--- atable/atable/settings.py
+++ atable/atable/settings.py
@@ -104,7 +104,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
