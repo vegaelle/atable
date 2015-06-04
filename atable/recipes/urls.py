62083d39deadf7ebe4ead52b27ce910cd2e588c6
opi <opi@no-log.org>
2015-06-05 00:09:08 +0200

    Use django url template tag.

--- atable/recipes/urls.py
+++ atable/recipes/urls.py
@@ -4,8 +4,10 @@ from .views import RecipeListView, RecipeDetailView
 
 
 urlpatterns = [
-    url(r'^recipes$', RecipeListView.as_view()),
-    url(r'^recipes/(?P<pk>[-\w]+)$', RecipeDetailView.as_view()),
+    url(r'^$', RecipeListView.as_view(), name='home'), # temp
+    url(r'^recipes$', RecipeListView.as_view(), name='recipe_list'),
+    url(r'^recipes/(?P<pk>[-\w]+)$', RecipeDetailView.as_view(),
+         name='recipe_detail'),
     url(r'^roadmap/meal/(?P<meal_id>\d+)$', views.roadmap_meal,
          name='roadmap_meal'),
     url(r'^roadmap/session/(?P<session_id>\d+)$', views.roadmap_session,
